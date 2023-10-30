d0ead214b521b050c536f0d3267ede1b3c5c2e45
Raynier Perez Minyety <rayniercraft@gmail.com>
2023-10-30 15:30:23 -0400

    Total improvement

--- script.js
+++ script.js
@@ -6,37 +6,77 @@
         document.body.style.flexWrap = 'wrap';
         document.body.style.margin = '0';
         document.body.style.height = '100vh';
-    
-        document.body.innerHTML = '';
-    
-        let screenWidth = window.innerWidth, 
-                screenHeight = window.innerHeight,
-                X_QUANTITY = 10,
-                SquareSideSize = screenWidth / X_QUANTITY,
-                Y_QUANTITY = screenHeight / SquareSideSize;
+        document.body.style.overflow = 'hidden'
+        function main() {
+            while (true) {
             
-        for (let i = 0; i < (X_QUANTITY * Y_QUANTITY); i++) {
-        document.body.innerHTML += `<div
-            style='width: ${SquareSideSize}px; 
-            height: ${SquareSideSize}px'> </div>`;
+                const response = prompt("Inserte el numero de cuadros en horizontal"),
+                    X_QUANTITY = Number(response);
+
+                if (!response) {
+                    alert("Que pena que no quiera generar nada, si cambia de opinion, recargue la pagina")
+                    break;
+                }
+
+                if (isNaN(X_QUANTITY)) {
+                    alert('Inserte un numero valido');
+                    continue;
+                }
+                canvaInitialization(X_QUANTITY);
+                generateSquares();
+                document.addEventListener('keydown', generateSquares)
+                window.addEventListener('resize', () => actualizarCanva(X_QUANTITY));
+                break;
+            }
+        }
+            
+        function canvaInitialization(X_QUANTITY = 10) {
+            document.body.innerHTML = '';
+        
+            let screenWidth = window.innerWidth, 
+                    screenHeight = window.innerHeight,
+                    SquareSideSize = Math.floor(screenWidth / X_QUANTITY),
+                    Y_QUANTITY = screenHeight / SquareSideSize;
+                
+            for (let i = 0; i < (X_QUANTITY * Y_QUANTITY); i++) {
+                // document.body.innerHTML += `<div
+                //     style='width: ${SquareSideSize}px; 
+                //     height: ${SquareSideSize}px'> </div>`;
+                let node = document.createElement('div');
+                node.style.width = SquareSideSize+'px';
+                node.style.height = SquareSideSize+'px';
+
+                document.body.appendChild(node)
+            }
+
+            alert("Carga completa");
+        }
+
+        function actualizarCanva() {
+            let timer;
+            clearTimeout(timer)
+            timer = setTimeout(() => {
+                canvaInitialization();
+            }, 500)
         }
     
         function generateSquares(event) {
     
-            if (event.which !== 13) return;
-            console.log('enter')
+            if (!event || event.which === 13) {
+                document.querySelectorAll('div')
+                    .forEach(elemento => randomBg(elemento))
+            }
             
-            document.querySelectorAll('div')
-                .forEach(elemento => randomBg(elemento))
         }
     
         async function randomBg(element) {
-            let number = Math.round(Math.random() * 1000);
+            let number = Math.round(Math.random() * parseInt('FFFFFF', 16));
             element.style.backgroundColor = '#' + number.toString(16);
             
         }
 
 
-        document.addEventListener('keydown', generateSquares)
+        main();
+
     })
 })();
